b561cbf0240be5a227f1f7f3e03a96d76673c6c8
Daniel Papp <dpapp001@odu.edu>
2019-06-18 14:10:16 -0400

    completed search fn
    will be merged to master once I can add the results into the main page/styling properly

--- src/search.php
+++ src/search.php
@@ -6,21 +6,20 @@ require_once '../build/components/globals.php';
 
 $searchTerm = $_GET['term'];
 
-$sql  = 'SELECT * FROM `module` WHERE `name` LIKE "%'.$searchTerm.'%" ORDER BY `uid` DESC';
+$sql  = "SELECT * FROM module WHERE name LIKE '%$searchTerm%'";
 
-$query = $conn->query($sql);
-
-$skillData = array();
+$query = $conn->query($sql) or die("MySQL error, query error");
 
 if($query->num_rows > 0){
     while($row = $query->fetch_assoc()){
-        $termData['uid'] = $row['uid'];
-        $termData['value'] = $row['name'];
-        array_push($skillData, $termData);
+        echo $row['uid'] . " " . $row['name'];
+        if($row['author'] != NULL){
+            echo " by " . $row['author'] . "<br>";
+        }
     }
 }
 
-echo json_encode($skillData);
+// echo json_encode($skillData);
 
 require_once '../build/components/footer.php'
 ?>
