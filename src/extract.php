e18a8619f48c4402b48c816876334412736101f7
Daniel Papp <dpapp001@odu.edu>
2019-11-04 14:41:15 -0500

    feat(upload): allowing folder upload lms-96 add modules via folder upload

--- src/extract.php
+++ src/extract.php
@@ -7,11 +7,10 @@ loginCheck();
     <div class="row">
         <div class="col mr-auto ml-auto">
             <h3 class="text-center">Set up extraction</h3>
-            <form class="form-group d-flex justify-content-center flex-column text-center" action="review.php" method="post">
-                <label class="w-100" for="">Folder name of the project that is being added</label>
-                <input class="w-100 form-control" maxlength="250" type="text" placeholder="Enter folder name" name="link" required>
-
-                <button class="w-25 mr-auto ml-auto mt-5 btn btn-primary" type="submit">Submit</button>
+            <form class="form-group d-flex justify-content-center flex-column text-center" action="../templates/folder.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="folderName">
+                <input type="file" name="folder[]" multiple="" directory="" webkitdirectory="" mozdirectory="">
+                <button class="w-25 mr-auto ml-auto mt-5 btn btn-primary" type="submit" name="submit">Submit</button>
             </form>
         </div>
     </div>
