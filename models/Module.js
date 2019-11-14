24e27a2493bf32067477a6351482566f1698e4ec
Daniel Papp <dpapp001@odu.edu>
2019-11-14 14:25:04 -0500

    feat(schema): added module schema
    #7 Done
    #25 In Progress

--- models/Module.js
+++ models/Module.js
@@ -1,35 +1,119 @@
 const mongoose = require('mongoose')
 
 const Module = new mongoose.Schema({
-    firstName: {
+    courseName: {
         type: String,
         trim: true,
         default: '',
-
     },
-    lastName: {
+    courseNumber: {
+        type: Number,
+        default: 0,
+    },
+    moduleNumber: {
+        type: Number,
+        default: 0,
+    },
+    moduleName: {
         type: String,
         trim: true,
         default: '',
-
     },
-    age: {
+    description: {
+        type: String,
+        default: ''
+    },
+    duration: {
+        type: Number,
+        default: 0
+    },
+    cdLink: {
+        type: String,
+        trim: true,
+        default: './'
+    },
+    numSlides: {
         type: Number,
         trim: true,
         default: 0
     },
-    team: {
+    author: {
         type: String,
         trim: true,
-        default: '',
-
+        default: ''
     },
-    position: {
-        type: String,
-        trim: true,
-        default: '',
+    difficulty: {
+        type: Number,
+        default: 0
+    },
+    done: {
+        type: Boolean
+    },
+    continue: {
+        type: Boolean
+    },
+    rating: [
+        Number
+    ],
+    keywords: [
+        String
+    ],
+    assignment: {
+        //specifies if there is an assignment for the module
+        type: Boolean
+    },
+    numberOfAssignments: {
+        //specifies the number of assignments for given module
+        type: Number
+    },
+    listOfAssignments: [{
+        //contains the list of assignments
+        assignment: [{
+            //one specific assignment in the array
+            id: {
+                type: Number,
+                unique: true 
+            },
+            title: String,
+            description: String,
+            topics: [ 
+                //specifies the topics covered by this specific assignment
+                Array 
+            ],
+            numberOfQuestions: Number,
+            PassingGrade: { 
+                type: Number, 
+                default: 0
+            },
+            question:[{
+                //one specific question in the assignment
+                number: Number,
+                title: String,
+                problem: String,
+                correctAnswer: String,
+                response: String,
+                point: Number,
+                visual: Boolean,
+                resource: [{
+                    //available resource for specific question
+                    link: String,
+                    name: String
+                }],
+                relates: Number, //specifies the UUID of the module that has this question's help/answer
+            }],
+            retake: Boolean,
+            due: Date,
+            allowedTime: Number,
 
-    }
+        }]
+    }],
+    preTest:{
+        type: Boolean
+    },
+    postTest: {
+        type: Boolean
+    },
+
 })
 
 module.exports = mongoose.model('Module', Module)
