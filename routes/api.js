c3bed2ea712f4f376ffcc30306b9cc308d88847f
Daniel Papp <dpapp001@odu.edu>
2019-11-15 11:50:23 -0500

    feat(routes): added query

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,19 @@ router.get('/users', (req, res) => {
 	})
 })
 
+//Query record based on difficulty grater than filter
 router.get('/modules', (req, res) => {
-	Module.find()
+	let filters = req.query
+
+	if(req.query.difficulty != null){
+		filters = {
+			difficulty: {
+				$gt: req.query.difficulty
+			}
+		}
+	}
+
+	Module.find(filters)
 	.then(modules => {
 		res.json({
 			confirmation: 'success',
@@ -38,5 +49,32 @@ router.get('/modules', (req, res) => {
 	})
 })
 
+//Query record by ID
+router.get('/modules/:id', (req, res) => {
+	const id = req.params.id
+
+	Module.findById(id)
+	.then(modules => {
+		res.json({
+			confirmation: 'success',
+			data: modules
+		})
+	})
+	.catch( err => {
+		res.json({
+			confirmation: 'fail',
+			message: `Module ${id} not found. Please check your search term.`  
+		})
+	})
+})
+
+//Add user profile
+router.post('/profile'), (req, res) => {
+	//TODO: Complete the form in handlebars and get post from it to insert into document
+	res.json({
+		confirmation: 'success',
+		data: req.body
+	})
+}
 
 module.exports = router
