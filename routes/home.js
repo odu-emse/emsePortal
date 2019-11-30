7b1ab31b87123cd6a4a0ff05b3b7449631160932
Daniel <dpapp001@odu.edu>
2019-11-30 10:59:49 -0500

    feat(conditional): added view conditions

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,7 @@ const home = express.Router()
 //Query record based on difficulty grater than filter
 home.get('/', (req, res) => {
   res.render('index', {
+    activeHome: true,
     title: 'Home', 
     loggedOut: false, 
     admin: false
