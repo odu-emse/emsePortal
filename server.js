d919bb5be53138b8f8ed5dd97f1d9430b9aa2737
Daniel <dpapp001@odu.edu>
2020-01-14 12:21:06 -0500

    feat(es6): switched to es6 functionality

--- server.js
+++ server.js
@@ -1,52 +1,82 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const cookieParser =  require('cookie-parser');
-const sassMiddleware = require('node-sass-middleware');
-const mongoose = require('mongoose');
-const hbs = require('express-handlebars')
+import express from 'express'
+import path from 'path'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+import sassMiddleware from 'node-sass-middleware'
+import postcssMiddleware from 'postcss-middleware'
+import autoprefixer from 'autoprefixer'
+import mongoose from 'mongoose'
+import hbs from 'express-handlebars'
+import createError from 'http-errors'
+import passport from 'passport'
+import Strategy from 'passport-local'
+import session from 'express-session'
+import flash from 'connect-flash'
 const app = express();
+import * as helpers from './helper/helpers'
+
+//Router imports
+import {home as home} from './routes/home'
+import {modules as modules} from './routes/modules'
+import {users as users} from './routes/users'
+import {signup as signup} from './routes/signup'
 
 //Database connection
-mongoose.connect('mongodb+srv://root:root@emseportal-1qgvd.mongodb.net/test?retryWrites=true&w=majority', {
+const url = "mongodb+srv://root:root@emseportal-1qgvd.mongodb.net/test?retryWrites=true&w=majority"
+const options = {
   'useUnifiedTopology': true,
   'useNewUrlParser': true,
   'useCreateIndex': true
-})
-  .then(() => console.log('Connected to database...'))
-  .catch(err => console.error(err));
+}
+mongoose.connect(url, options)
+  .then(() => {
+    console.log('Connected to db... ');
+    
+  })
+  .catch((err) => {
+    console.error(err);
+  })
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}))
+app.engine('hbs', hbs({
+  helpers: helpers.helpers,
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts/'
+}))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 //Middleware
 //server middleware
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //Sass middleware
-let sassPath = path.join(__dirname, '/sass');
-let destPath = path.join(__dirname, '/public/style');
+let dest = __dirname + '/public'
 app.use(sassMiddleware({
   src: __dirname,
-  dest: __dirname + '/public',
+  dest: dest,
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true,
   debug: true, 
 }));
+app.use(postcssMiddleware({
+  plugins: [
+    /* Plugins */
+    autoprefixer()
+  ],
+  src: function(req) {
+    return path.join(dest, req.url);
+  }
+}))
 
 //Static middleware
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'))
 
 //Controller
-const modules = require('./routes/modules')
-const users = require('./routes/users')
-const home = require('./routes/home')
-const signup = require('./routes/signup')
 
 //Routes
 app.use('/modules', modules);
@@ -54,8 +84,20 @@ app.use('/users', users);
 app.use('/', home);
 app.use('/signup', signup);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-//Server
-app.listen(5000, () => console.log('Server has started on port 3000...'));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = app
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app
