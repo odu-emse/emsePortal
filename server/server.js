943cb1cd28f810a3cf3c5f452e9ba6f8ed99aefd
Daniel Papp <dpapp001@odu.edu>
2020-05-04 15:25:24 -0400

    fix(deploy): fix dev and staging deployment procedure

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ import path from "path";
 import passport from "passport";
 import Cors from "cors";
 import { database } from "./config/db";
-import flash from "connect-flash";
-import session from "express-session";
 
 require("./config/passport");
 
@@ -15,23 +13,8 @@ app.use(Cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use(
-//   session({
-//     secret: "secret",
-//     resave: true,
-//     saveUninitialized: true
-//   })
-// );
-
 app.use(passport.initialize());
 app.use(passport.session());
-
-// app.use(flash());
-// app.use((req, res, next) => {
-//   res.locals.success = req.flash("success");
-//   res.locals.error = req.flash("error");
-//   next();
-// });
 
 //Router imports
 import course from "./routes/api/course";
@@ -55,5 +38,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}...`)
+);
 
 export default app;
