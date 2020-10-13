5717cb2d7eae2db153607f8e0d66128cd2acb842
Daniel Papp <dpapp001@odu.edu>
2020-10-12 20:11:55 -0400

    feature(modules): updated UI for module browsing

--- src/components/modules/ModuleItem.js
+++ src/components/modules/ModuleItem.js
@@ -1,173 +1,126 @@
-import React, { Suspense } from "react"
-import { fetchData } from "./ModuleFetch"
-import {
-	Typography,
-	Card,
-	CardContent,
-	CardActions,
-	Container,
-	Button,
-	ThemeProvider,
-} from "@material-ui/core"
+import React, { useEffect, useState } from "react"
+import { Typography, Container, Grid, Link } from "@material-ui/core"
 import StarRatingComponent from "react-star-rating-component"
-import { rating, round_to_precision, progress } from "../helpers"
+import { rating, round_to_precision } from "../helpers"
 import { Loader } from "react-feather"
+import Unsplash, { toJson } from "unsplash-js"
+import axios from "axios"
 
-//storing fetch data return in resource protected variable for suspense
-const resource = fetchData()
+const unsplash = new Unsplash({ accessKey: process.env.REACT_APP_IMAGE_ACCESS })
 
 let ModuleItem = () => {
-	return (
-		<Suspense
-			fallback={
-				<Container className="mx-auto w-100 d-flex justify-content-center align-items-center">
-					<Loader className="spin" size="42pt" />
-				</Container>
-			}
-		>
-			<ModuleData />
-		</Suspense>
-	)
-}
+	//initial state is here since api requires are limited
+	const initialImage = {
+		urls: {
+			thumb: "https://unsplash.com/photos/WpbEhnhuXrI",
+		},
+		description: "lorem",
+	}
+	const [image, setImage] = useState(null)
+	const [modules, setModules] = useState(null)
 
-const ModuleData = () => {
-	const modules = resource.module.read()
+	useEffect(() => {
+		axios
+			.get(`${process.env.REACT_APP_API}/api/modules`, {
+				headers: {
+					"Content-Type": "application/json",
+					"Cache-Control": "no-cache",
+				},
+			})
+			.then((res) => {
+				let results = res.data
+				console.log(res)
+				setModules(results)
+			})
+			.catch((err) => console.log(err))
 
-	return (
+		unsplash.photos
+			.getRandomPhoto({ orientation: "landscape" })
+			.then(toJson)
+			.then((json) => {
+				console.log(json)
+				setImage(json)
+			})
+			.catch((err) => {
+				console.error(err)
+			})
+	}, [])
+
+	const filterModules = (modules, text, variant) => {
+		if (variant === "full") {
+		} else if (variant === "featured") {
+		} else if (variant === "related") {
+		}
+		return (
+			<>
+				<h4 className="mt-3">{text}</h4>
+				<div className="module--list">
+					{modules.data.map((module) => (
+						<Link href={`/modules/${module._id}`}>
+							<div className="module--card shadow rounded">
+								<img
+									alt={
+										image === null
+											? ""
+											: image.alt_description
+									}
+									src={image === null ? "" : image.urls.thumb}
+									title={
+										image === null
+											? ""
+											: image.alt_description
+									}
+									className="module--card__image card-img-top"
+								/>
+								<div className="module--card__content">
+									<h4 className="text-bold">
+										{module.moduleName}
+									</h4>
+									<h6 className="text-muted">
+										{module.instructor}
+									</h6>
+									<Grid container className="w-100">
+										<StarRatingComponent
+											name="module-rating"
+											starCount={5}
+											editing={false}
+											value={round_to_precision(
+												rating(module.rating),
+												0.5
+											)}
+										/>
+										<p className="text-muted mx-2 font-weight-light">{`${round_to_precision(
+											rating(module.rating),
+											0.5
+										)} (${module.rating.length})`}</p>
+										<div className="flex flex-row">
+											{module.keywords.map((keyword) => (
+												<span className="badge badge-pill badge-secondary mx-1">
+													{keyword}
+												</span>
+											))}
+										</div>
+									</Grid>
+								</div>
+							</div>
+						</Link>
+					))}
+				</div>
+			</>
+		)
+	}
+
+	return modules === null ? (
+		<Container className="mx-auto w-100 d-flex justify-content-center align-items-center">
+			<Loader className="spin" size="42pt" />
+		</Container>
+	) : (
 		<>
-			{RemainingModules(modules)}
-			{IncompleteModules(modules)}
+			{filterModules(modules, "Where you left off")}
+			{filterModules(modules, "Featured modules")}
+			{filterModules(modules, "Because you completed X module")}
 		</>
 	)
 }
 
-//Pick up new modules
-const RemainingModules = (modules) => {
-	//filtering out modules that aren't done yet and storing in variable remaining
-	let remaining = modules.data.filter((module) => module.done === false)
-
-	const seen = new Set()
-	const uniqueData = remaining.filter(({ sid }) => {
-		if (seen.has(sid)) {
-			return false
-		}
-		seen.add(sid)
-		return true
-	})
-
-	return (
-		<ThemeProvider>
-			<Container>
-				<Typography variant="h4" className="module--title">
-					Pick up new modules
-				</Typography>
-				<div className="row">
-					<div className="col">Module name</div>
-					<div className="col">Rating</div>
-					<div className="col">Enrolled</div>
-					<div className="col">Actions</div>
-				</div>
-			</Container>
-			{uniqueData.map((module) => (
-				<Card
-					className="module--list--item"
-					key={module.moduleNumber}
-					id={module._id}
-				>
-					<CardContent className="d-flex align-items-center w-100">
-						<div className="row w-100 align-items-center">
-							<div className="col">{module.moduleName}</div>
-							<div
-								className="col d-flex align-items-center"
-								title={`Rating: ${rating(module.rating)}`}
-							>
-								<StarRatingComponent
-									editing={false}
-									name={`${module._id}Rating`}
-									starCount={5}
-									value={round_to_precision(
-										rating(module.rating),
-										0.5
-									)}
-								/>
-							</div>
-							<div className="col">{module.enrolled}</div>
-							<div className="col actions d-flex flex-row">
-								<CardActions>
-									<Button
-										color="primary"
-										className="mx-2"
-										size="small"
-										onClick={() => {
-											console.log(`accessed`)
-										}}
-									>
-										Add module
-									</Button>
-								</CardActions>
-							</div>
-						</div>
-					</CardContent>
-				</Card>
-			))}
-		</ThemeProvider>
-	)
-}
-
-//Continue modules
-const IncompleteModules = (modules) => {
-	let completed = modules.data.filter((module) => module.continue === true)
-
-	const seen = new Set()
-	const uniqueData = completed.filter(({ sid }) => {
-		if (seen.has(sid)) {
-			return false
-		}
-		seen.add(sid)
-		return true
-	})
-
-	return (
-		<ThemeProvider>
-			<Typography variant="h4" className="module--title">
-				Continue your modules
-			</Typography>
-			<div className="row module--list">
-				<div className="col">Module name</div>
-				<div className="col">Progress</div>
-				<div className="col">Remaining</div>
-				<div className="col">Actions</div>
-			</div>
-
-			{uniqueData.map((module) => (
-				<Card
-					className="module--list--item"
-					key={module.moduleNumber}
-					id={module._id}
-				>
-					<CardContent className="d-flex align-items-center w-100">
-						<div className="row w-100 align-items-center">
-							<div className="col">{module.moduleName}</div>
-							<div className="col">
-								{progress(module.duration, module.remaining)}%
-							</div>
-							<div className="col">{module.duration} minutes</div>
-							<div className="col actions d-flex flex-row">
-								<Button
-									href={`modules/${module._id}`}
-									className="module--accessButton mx-2"
-									size="small"
-									color="primary"
-									variant="outlined"
-								>
-									Continue module
-								</Button>
-							</div>
-						</div>
-					</CardContent>
-				</Card>
-			))}
-		</ThemeProvider>
-	)
-}
 export default ModuleItem
