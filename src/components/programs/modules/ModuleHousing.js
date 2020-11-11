f17f10c6e1553ba6b00c826014451073c412817a
Daniel Papp <dpapp001@odu.edu>
2020-11-11 16:17:38 -0500

    feature(program): users can enroll into modules
    - enrollment is stored in the database based on user ID

--- src/components/programs/modules/ModuleHousing.js
+++ src/components/programs/modules/ModuleHousing.js
@@ -2,43 +2,147 @@ import React, { useEffect, useState } from "react"
 import axios from "axios"
 import { Container } from "@material-ui/core"
 import { Loader } from "react-feather"
+import { decoder, round_to_precision, rating } from "../../helpers"
+import StarRatingComponent from "react-star-rating-component"
+import pluralize from "pluralize"
+import { ToastContainer, toast } from "react-toastify"
 
 export default function ModuleHousing(props) {
-	const initialUserState = {
-		module: {},
-		loading: true,
-	}
-
 	const {
 		match: { params },
 	} = props
 
-	const [module, setUser] = useState(initialUserState)
+	const [module, setModule] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [enrolled, setEnrolled] = useState(false)
+
+	const addModule = (id) => {
+		axios
+			.put(
+				`${
+					process.env.REACT_APP_API
+				}/api/users/${decoder()}?module=${id}`,
+				{},
+				{}
+			)
+			.then(() => {
+				window.location.reload()
+				toast.success("You have successfully enrolled", {
+					position: toast.POSITION.TOP_RIGHT,
+				})
+			})
+			.catch((err) => {
+				console.error(err)
+			})
+	}
 
 	useEffect(() => {
-		const getUser = async () => {
-			let config = {
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-			const { data } = await axios(
+		let config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+		axios
+			.get(
 				`${process.env.REACT_APP_API}/api/modules/${params.moduleId}`,
 				config
 			)
-
-			setUser(data)
-		}
-
-		getUser()
-	}, [])
-	return module.loading ? (
+			.then((response) => {
+				const module = response.data.data
+				setModule(module)
+				if (module.enrolled.includes(decoder())) {
+					setEnrolled(true)
+				} else {
+					setEnrolled(false)
+				}
+				setLoading(false)
+			})
+			.catch((err) => {
+				console.error(err)
+			})
+	}, [params.moduleId])
+	return loading ? (
 		<Container className="mx-auto w-100 d-flex justify-content-center align-items-center">
 			<Loader className="spin" size="42pt" />
 		</Container>
+	) : enrolled ? (
+		<>
+			<ToastContainer />
+			<div className="container">
+				<h1>{module.moduleName}</h1>
+			</div>
+		</>
 	) : (
-		<div className="container">
-			<h1>{module.data._id}</h1>
-		</div>
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-9">
+						<h1>
+							Module {module.moduleNumber} | {module.moduleName}
+						</h1>
+						<p>{module.description}</p>
+						<div className="module--rating row w-50">
+							<p className="font-weight-light col text-warning d-flex align-items-center">
+								<span className="pr-2">{`${round_to_precision(
+									rating(module.rating),
+									0.5
+								)}`}</span>
+								<StarRatingComponent
+									name="module-rating"
+									starCount={5}
+									editing={false}
+									value={round_to_precision(
+										rating(module.rating),
+										0.5
+									)}
+								/>
+								<span className="pl-2 text-muted">
+									({module.rating.length} ratings)
+								</span>
+								<span className="pl-2 text-muted">
+									{pluralize(
+										"student",
+										module.enrolled.length,
+										true
+									)}
+								</span>
+							</p>
+						</div>
+						<p>
+							Instructed by{" "}
+							<a className="underline" href="./">
+								{module.instructor}
+							</a>
+						</p>
+						<div>
+							<h3>What you'll learn</h3>
+							<p>list of module objectives here</p>
+						</div>
+					</div>
+					<div className="col-md-3">
+						<img
+							src={`${module.cdLink}/story_content/thumbnail.jpg`}
+							alt={`${module.moduleName} thumbnail`}
+						/>
+						<button
+							onClick={(e) => {
+								addModule(module._id)
+							}}
+							className="btn btn-secondary"
+						>
+							Add module to your curriculum
+						</button>
+						<div className="my-4">
+							<h5>This course will cover:</h5>
+							<div>
+								{module.keywords.map((key, index) => (
+									<p key={index}>{key}</p>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import * as Sentry from '@sentry/react'
+import React from "react"
+import ReactDOM from "react-dom"
+import * as Sentry from "@sentry/react"
 import { Integrations } from "@sentry/tracing"
-import App from "./App";
+import App from "./App"
 
-if(process.env.REACT_APP_INSTANCE === 'production'){
-Sentry.init({
-    dsn: "https://e6d7145b40b34160b57eec2ad3f663d8@o452753.ingest.sentry.io/5440649",
-    integrations: [
-        new Integrations.BrowserTracing(),
-    ],
-    tracesSampleRate: 1.0,
-});
+if (process.env.REACT_APP_INSTANCE === "production") {
+	Sentry.init({
+		dsn:
+			"https://e6d7145b40b34160b57eec2ad3f663d8@o452753.ingest.sentry.io/5440649",
+		integrations: [new Integrations.BrowserTracing()],
+		tracesSampleRate: 1.0,
+	})
 }
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 )
 
-RegExp.escape = function(s) {
-    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-};
+RegExp.escape = function (s) {
+	return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")
+}
