2480dbd4c7b31512f0e71204c91d11415e9dabe6
Dániel B. Papp <dpapp@odu.edu>
2022-05-11 19:07:54 -0400

    fix(ui): implemented search functionality
    Users can now search through modules using MongoDB Realm and Atlas Index search.
    
    BREAKING CHANGE: Done ALMP-220

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,89 +3,113 @@ import Dialogue from './Dialogue'
 import { Search as SearchIcon } from 'react-feather'
 import { AiOutlineSearch } from 'react-icons/ai'
 import axios from 'axios'
+import { loader } from '../helpers'
 
 const Search = () => {
 	const [search, setSearch] = useState('')
-	const [select, setSelect] = useState('')
 	const [results, setResults] = useState()
+	const [open, setOpen] = useState(false)
 
-	const getResults = async (event) => {
+	const getResults = async event => {
 		event.preventDefault()
-		try {
-			const res = await axios.get(
-				`https://us-east-1.aws.data.mongodb-api.com/app/emseedge-qbmoy/endpoint/search?term=${search}&field=${select}`
-			)
-			setResults(res.data)
-		} catch (error) {
-			console.error(error)
-			return null
+		if (search.length > 0) {
+			try {
+				const res = await axios.get(
+					`${process.env.REACT_APP_DB_URL}/search?term=${search}`
+				)
+				setResults(res.data)
+				setOpen(true)
+			} catch (error) {
+				console.error(error)
+				throw new Error(error)
+			}
 		}
 	}
 
-	const change = (e) => {
-		switch (e.target.value) {
-			case 'title':
-				setSelect('moduleName')
-				break
-			case 'number':
-				setSelect('moduleNumber')
-				break
-			case 'key':
-				setSelect('keywords')
-				break
-			case 'desc':
-				setSelect('description')
-				break
-			case 'assignment':
-				break
-			default:
-				break
+	const handleInputChange = event => {
+		setSearch(event.target.value)
+		setOpen(false)
+		if (search.length === 0 && results) {
+			setResults(null)
+			setOpen(false)
 		}
 	}
-
-	console.log(results)
 
 	return (
 		<div className="relative">
 			<section className="my-5 mb-0 w-full flex items-center justify-between">
 				<div className="w-full relative">
 					<form
-						onSubmit={(e) => getResults(e)}
+						onSubmit={e => getResults(e)}
 						className="flex items-center w-full"
 					>
 						<button type="submit" className="flex items-center">
-							<AiOutlineSearch className="text-5xl opacity-50 absolute left-0" />
+							<AiOutlineSearch className="text-5xl opacity-50 absolute left-2" />
 						</button>
 						<input
-							onChange={(e) => setSearch(e.target.value)}
+							onChange={e => handleInputChange(e)}
 							type="text"
 							value={search}
-							className="border-gray-300 shadow rounded mr-3 w-full text-5xl px-14 py-2"
+							className="border-gray-300 shadow rounded w-full text-5xl px-14 py-2"
 							placeholder="What are you looking for"
 						/>
 					</form>
 				</div>
-				<select name="field" onChange={(e) => change(e)}>
-					<option value="title">Title</option>
-					<option value="number">Number</option>
-					<option value="key">Keyword</option>
-					<option value="desc">Description</option>
-					<option value="assg">Assignment</option>
-				</select>
 			</section>
-			{search ? (
-				<div className="absolute left-0 bottom-0 top-24 bg-gray-50 w-full h-32 shadow px-4 py-1 border border-gray-300">
-					{results && results.length !== 0 ? (
-						<>
-							{results.map((result) => (
-								<div className="">
-									<p>{result.moduleNumber} | </p>
-									<p>{result.moduleName}</p>
-								</div>
-							))}
-						</>
+			{open ? (
+				<div className="absolute left-0 bottom-0 top-24 bg-gray-50 w-full h-fit shadow border border-gray-300">
+					{results ? (
+						results.length !== 0 ? (
+							<>
+								{results.map((result, i) => (
+									<div
+										className="px-2 py-4 flex border items-center justify-between border-transparent hover:bg-gray-300 hover:border-gray-400"
+										key={i}
+									>
+										<div className="">
+											<div className="flex items-center text-xl font-semibold mb-3">
+												<p className="mr-1">
+													M-
+													{
+														result.moduleNumber
+															.$numberLong
+													}{' '}
+													|
+												</p>
+												<p>{result.moduleName}</p>
+											</div>
+											<div className="">
+												{result.keywords.map(
+													(keyword, index) => (
+														<span
+															className="bg-blue-300 rounded-full px-4 py-1 mx-1 inline-block font-light"
+															key={index}
+														>
+															{keyword}
+														</span>
+													)
+												)}
+											</div>
+										</div>
+										<div className="italic text-gray-500 font-light font-serif text-sm">
+											{Number(
+												result.score.$numberDouble * 100
+											).toFixed(2)}
+											% match
+										</div>
+									</div>
+								))}
+							</>
+						) : (
+							<p className="text-lg">
+								No results found for:{' '}
+								<em className="text-blue-800 font-semibold">
+									{search}
+								</em>
+							</p>
+						)
 					) : (
-						<p>No results found for...</p>
+						loader()
 					)}
 				</div>
 			) : null}
--- src/components/users/PlanOfStudy.js
+++ src/components/users/PlanOfStudy.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { getModule, loader } from '../helpers'
 import axios from 'axios'
+import moment from 'moment'
 
 const PlanOfStudy = ({ param }) => {
 	const [lessons, setLessons] = useState([])
+	const [enrollment, setEnrollment] = useState(null)
+	const [courses, setCourses] = useState(null)
+	const [assignments, setAssignments] = useState(null)
 	const [loading, setLoading] = useState(true)
 
 	const getPlan = async () => {
@@ -11,11 +15,46 @@ const PlanOfStudy = ({ param }) => {
 			query: `{
                         planByID(id: "${param}"){
                             id,
-                            student{
-                                id,
-                                firstName,
-                                lastName
-                            }
+							student{
+								id,
+								email,
+								firstName,
+								lastName,
+								dob
+							},
+							modules{
+								id,
+								enrolledAt,
+								role,
+								module{
+									id,
+									moduleName,
+									moduleNumber
+								},
+							},
+							courses{
+								id,
+								enrolledAt,
+								course{
+									name,
+								}
+							},
+							assignmentResults{
+								id,
+								assignment{
+									id,
+									name,
+									dueAt,
+								},
+								gradedBy{
+									id,
+									email,
+									firstName,
+									lastName
+								},
+								result,
+								submittedAt
+							}
                         }
                     }`,
 		}
@@ -25,10 +64,11 @@ const PlanOfStudy = ({ param }) => {
 					'Content-Type': 'application/json',
 				},
 			})
-			.then((document) => {
+			.then(document => {
+				setLoading(false)
 				return document.data.data.planByID
 			})
-			.catch((err) => {
+			.catch(err => {
 				console.error(err)
 			})
 		return plan
@@ -36,27 +76,31 @@ const PlanOfStudy = ({ param }) => {
 
 	useEffect(() => {
 		getPlan()
-			.then((response) => {
+			.then(response => {
 				try {
-					response?.modules.forEach((id) => {
-						getModule(id)
-							.then((resp) => {
-								setLessons((lessons) => [
-									...lessons,
-									resp.data.data,
-								])
-								setLoading(false)
-							})
-							.catch((err) => {
-								console.error(err)
-							})
-					})
+					setEnrollment(response.modules)
+					setCourses(response.courses)
+					setAssignments(response.assignmentResults)
+					// TODO: ALMP-222
+					// response?.modules.forEach((id) => {
+					// 	getModule(id)
+					// 		.then((resp) => {
+					// 			setLessons((lessons) => [
+					// 				...lessons,
+					// 				resp.data.data,
+					// 			])
+					// 			setLoading(false)
+					// 		})
+					// 		.catch((err) => {
+					// 			console.error(err)
+					// 		})
+					// })
 				} catch (error) {
 					console.error(error)
 					setLoading(false)
 				}
 			})
-			.catch((err) => {
+			.catch(err => {
 				console.log(err)
 			})
 	}, [])
@@ -65,71 +109,254 @@ const PlanOfStudy = ({ param }) => {
 		loader()
 	) : (
 		<>
-			<table className="border-collapse w-full">
-				<thead>
-					<tr>
-						<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-							Lesson number
-						</th>
-						<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-							Lesson name
-						</th>
-						<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-							Progress
-						</th>
-						<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-							Actions
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{lessons.map((lesson, index) => (
-						<tr
-							key={index}
-							className="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
-						>
-							<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-								<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-									Lesson number
-								</span>
-								{lesson.moduleNumber}
-							</td>
-							<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-								<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-									Lesson name
-								</span>
-								{lesson.moduleName}
-							</td>
-							<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-								<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-									Progress
-								</span>
-								{/* <span className="rounded bg-red-400 py-1 px-3 text-xs font-bold"> */}
-								some number that AI generates or whatever smart
-								people say... 🤠
-								{/* </span> */}
-							</td>
-							<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-								<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-									Actions
-								</span>
-								<a
-									href="/"
-									className="text-blue-400 hover:text-blue-600 underline"
-								>
-									Edit
-								</a>
-								<a
-									href="/"
-									className="text-blue-400 hover:text-blue-600 underline pl-6"
-								>
-									Remove
-								</a>
-							</td>
+			<div className="lg:mb-2">
+				<h1 className="text-lg font-semibold">My Modules</h1>
+				<table className="border-collapse w-full">
+					<thead>
+						<tr>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Enrolled At
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Role
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Module
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Progress
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Actions
+							</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{enrollment.map((enrollment, index) => {
+							let at = moment.unix(
+								~~(enrollment.enrolledAt / 1000)
+							)
+							const enrolledAt = {
+								day: at.format('DD'),
+								month: at.format('MM'),
+								year: at.format('YYYY'),
+								time: at.format('hh:mm A'),
+							}
+							return (
+								<tr
+									key={index}
+									className="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
+								>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Enrolled At
+										</span>
+										{`${enrolledAt.day}/${enrolledAt.month}/${enrolledAt.year} ${enrolledAt.time}`}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Role
+										</span>
+										{enrollment.role}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Module
+										</span>
+										{enrollment.module.moduleNumber} -{' '}
+										{enrollment.module.moduleName}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Progress
+										</span>
+										{/* <span className="rounded bg-red-400 py-1 px-3 text-xs font-bold"> */}
+										some number that AI generates or
+										whatever smart people say... 🤠
+										{/* </span> */}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Actions
+										</span>
+										<button className="bg-red-700 text-red-50 hover:text-red-200 hover:bg-red-800 px-6 py-2">
+											Abandon
+										</button>
+									</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
+			<div className="lg:mb-2">
+				<h1 className="text-lg font-semibold">My Courses</h1>
+				<table className="border-collapse w-full">
+					<thead>
+						<tr>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Enrolled At
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Course
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Actions
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{courses.map((course, index) => {
+							let at = moment.unix(~~(course.enrolledAt / 1000))
+							const enrolledAt = {
+								day: at.format('DD'),
+								month: at.format('MM'),
+								year: at.format('YYYY'),
+								time: at.format('hh:mm A'),
+							}
+							return (
+								<tr
+									key={index}
+									className="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
+								>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Enrolled At
+										</span>
+										{`${enrolledAt.day}/${enrolledAt.month}/${enrolledAt.year} ${enrolledAt.time}`}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Course
+										</span>
+										{course.course.name}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Actions
+										</span>
+										<button className="bg-red-700 text-red-50 hover:text-red-200 hover:bg-red-800 px-6 py-2">
+											Abandon
+										</button>
+									</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
+			<div className="lg:mb-2">
+				<h1 className="text-lg font-semibold">My Assignments</h1>
+				<table className="border-collapse w-full">
+					<thead>
+						<tr>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Due At
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Submitted At
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Assignment Name
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Graded By
+							</th>
+							<th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
+								Result
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{assignments.map((assignment, index) => {
+							console.log(assignment)
+							let at = moment.unix(
+								~~(assignment.submittedAt / 1000)
+							)
+							let due = moment.unix(
+								~~(assignment.assignment.dueAt / 1000)
+							)
+							const enrolledAt = {
+								day: at.format('DD'),
+								month: at.format('MM'),
+								year: at.format('YYYY'),
+								hour: at.format('HH'),
+								minute: at.format('mm'),
+								time: at.format('hh:mm A'),
+							}
+							const dueAt = {
+								day: due.format('DD'),
+								month: due.format('MM'),
+								year: due.format('YYYY'),
+								hour: due.format('hh'),
+								minute: due.format('mm'),
+								time: at.format('hh:mm A'),
+							}
+							return (
+								<tr
+									key={index}
+									className="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
+								>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Due At
+										</span>
+										{`${dueAt.day}/${dueAt.month}/${dueAt.year} ${dueAt.time}`}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Submitted At
+										</span>
+										{`${enrolledAt.day}/${enrolledAt.month}/${enrolledAt.year} ${enrolledAt.time}`}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Course
+										</span>
+										{assignment.assignment.name}
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Course
+										</span>
+										<a
+											href={`mailto:${assignment.gradedBy.email}?subject=Question about assignment ${assignment.assignment.name}`}
+											target="_blank"
+											rel="noreferrer"
+											className="underline text-blue-400"
+										>
+											{assignment.gradedBy.firstName}{' '}
+											{assignment.gradedBy.lastName}
+										</a>
+									</td>
+									<td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+										<span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+											Actions
+										</span>
+										<span
+											className={`${
+												assignment.result < 75
+													? 'text-red-600'
+													: null
+											} ${
+												assignment.result >= 75
+													? 'text-yellow-600'
+													: null
+											} ${
+												assignment.result >= 85
+													? 'text-green-600'
+													: null
+											}`}
+										>
+											{assignment.result}
+										</span>
+									</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
 		</>
 	)
 }
