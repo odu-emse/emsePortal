89aa74acc586969e1d2cfd55bc8c7d9593600cb2
Dániel B. Papp <dpapp@odu.edu>
2022-08-24 11:33:15 -0400

    style(UI): :lipstick: Modified the confirm password modal to have proper onClick handlers
    Closes ALMP-248

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -597,87 +597,114 @@ const Profile = (props) => {
 							</>
 						)}
 
-						
 						<button
-						className="bg-blue-300 border-blue-200 rounded w-auto text black px-4 py-2 m-2"
-						onClick={ (e) => {e.preventDefault()
-						setShowModal(!showModal)} }>
-							Change Password
+							className="bg-blue-300 border-blue-200 rounded w-auto text black px-4 py-2 m-2"
+							onClick={(e) => {
+								e.preventDefault()
+								setShowModal(!showModal)
+							}}
+						>
+							Update Profile
 						</button>
-						
-						<div className={`relative z-10 ${showModal ? 'visible' : 'invisible'}`} aria-labelledby="modal-title" role="dialog" aria-modal="true">	
+
+						<div
+							className={`relative z-10 ${
+								showModal ? 'visible' : 'invisible'
+							}`}
+							aria-labelledby="modal-title"
+							role="dialog"
+							aria-modal="true"
+						>
 							<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 							<div className="fixed z-10 inset-0 overflow-y-auto">
-								<div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">								
-									<div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+								<div className="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
+									<div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full w-2/3 max-w-screen-lg">
 										<div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-											<div className="sm:items-start">
-												<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-												<h3 className="text-lg leading-6 font-medium text-gray-900 my-2" id="modal-title">Update Password</h3>
-												<hr/>
-													<div className="w-full mb-3 gap-4">
+											<div className="mt-3 text-center sm:mt-0 sm:text-left">
+												<h3 className="text-lg leading-6 font-medium text-gray-900 my-2 border-b border-gray-200">
+													Confirm Password
+												</h3>
+												<form className="w-full mb-3 gap-4">
 													<label
-													htmlFor=""
-													className="block flex-1 font-bold"
+														htmlFor="password"
+														className="block flex-1 font-bold"
 													>
-													
-													<input
-														className="lg:basis-1/2 bg-gray-50 border border-gray-200 rounded shadow-sm py-2 px-2 block my-4 w-full"
-														type="password"
-														placeholder="New Password"
-														name="password"
-														defaultValue={user?.password}
-														required={true}
-														onChange={(e) =>
-														setUser({
-															...user,
-															password: e.target.value,
-														})
-														}
-													/>
+														<input
+															className="lg:basis-1/2 bg-gray-50 border border-gray-200 rounded shadow-sm py-2 px-2 block my-4 w-full"
+															type="password"
+															placeholder="New Password"
+															name="password"
+															defaultValue={
+																user?.password
+															}
+															required={true}
+															onChange={(e) =>
+																setUser({
+																	...user,
+																	[e.target
+																		.name]:
+																		e.target
+																			.value,
+																})
+															}
+														/>
 													</label>
 													<label
-													htmlFor=""
-													className="block flex-1 font-bold"
+														htmlFor="passwordConfirmation"
+														className="block flex-1 font-bold"
 													>
-													
-													<input
-														className="lg:basis-1/2 bg-gray-50 border border-gray-200 rounded shadow-sm py-2 px-2 block my-4 w-full"
-														type="password"
-														placeholder="Confirm Password"
-														name="passwordConf"
-														defaultValue={user?.passwordConf}
-														required={true}
-														onChange={(e) =>
-														setUser({
-															...user,
-															passwordConf: e.target.value,
-														})
-														}
-													/>
+														<input
+															className="lg:basis-1/2 bg-gray-50 border border-gray-200 rounded shadow-sm py-2 px-2 block my-4 w-full"
+															type="password"
+															placeholder="Confirm Password"
+															name="passwordConf"
+															defaultValue={
+																user?.passwordConf
+															}
+															required={true}
+															onChange={(e) =>
+																setUser({
+																	...user,
+																	[e.target
+																		.name]:
+																		e.target
+																			.value,
+																})
+															}
+														/>
 													</label>
+													<div className="sm:flex items-center justify-end">
+														<button
+															type="submit"
+															className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+															onClick={(e) => {
+																updateUser(e)
+																setShowModal(
+																	false
+																)
+															}}
+														>
+															Confirm
+														</button>
+														<button
+															type="reset"
+															className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-600 text-white text-base font-medium hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+															onClick={() =>
+																setShowModal(
+																	false
+																)
+															}
+														>
+															Cancel
+														</button>
 													</div>
-												</div>
+												</form>
 											</div>
-										</div>
-										<div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-											<button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
-											<button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Change</button>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-
-
-						
-						<button
-							className="bg-blue-300 border-blue-200 rounded w-auto text-black px-4 py-2"
-							onClick={(e) => updateUser(e)}
-							type="submit"
-						>
-							Update profile
-						</button>
 					</form>
 					<h3
 						id="modules"
