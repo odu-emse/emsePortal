217245d23c18329fe416c561ecfb880b6cbc250f
Joel DeSante <joeldesante@gmail.com>
2022-08-30 15:05:09 -0400

    Handle redirects

--- src/components/auth/Redirect.js
+++ src/components/auth/Redirect.js
@@ -1,5 +1,24 @@
+import { useEffect } from "react"
+import { useLocation } from "react-router";
+import login from "../../scripts/auth/login";
+
 const Redirect = () => {
-    return <div>Recieved</div>
+
+    let location = useLocation();
+    useEffect(() => {
+        const query = new URLSearchParams(location.search);
+        const code = query.get("code");
+
+        if (code == null) {
+            return
+        }
+
+        login(code)
+    });
+
+    return <div>
+        Recieved<br/>
+    </div>
 }
 
 export default Redirect
